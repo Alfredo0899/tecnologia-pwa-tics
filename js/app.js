b2a139dd994e42cea3cab17df905d29f59d77be6
fred95 <fredyalfredogomez1b@gmail.com>
2021-10-28 10:41:29 -0500

    Snapshot se ha modificado el nombre de la APP

--- js/app.js
+++ js/app.js
@@ -45,7 +45,7 @@ const showCoffees =()=>{
         <div class="card">
             <img class="card--avatar" src=${image} />
             <h1 class="card--title">${name}</h1>
-            <a class="card--link" href="#">Taste</a>
+            <a class="card--link" href="#">buscar</a>
         </div>
         `)
     );
